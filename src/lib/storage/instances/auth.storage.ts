e23c76cb46be9b213b2a581f6afdfe0c96d4d16c
HildodeJesus <hildo.pro.developer@gmail.com>
2026-04-13 11:32:44 -0300

    feat: add encryption key to auth storage configuration

--- src/lib/storage/instances/auth.storage.ts
+++ src/lib/storage/instances/auth.storage.ts
@@ -1,11 +1,14 @@
 import { createMMKV } from "react-native-mmkv";
 import type { AuthStorageSchema } from "@/types/storage";
 import { TypedStorage } from "../typed-storage";
+import { appConfig } from "@/lib/config";
 
 const mmkv = createMMKV({
   id: `auth-storage`,
   mode: "multi-process",
   readOnly: false,
+  encryptionKey: appConfig.encryptionKey
+
 });
 
 export const authStorage = new TypedStorage<AuthStorageSchema>(mmkv);
